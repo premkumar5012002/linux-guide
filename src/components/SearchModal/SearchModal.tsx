10ee93e9fda6de3c41fe9b8d58a367358fe174ec
Prem Kumar <premkumar5012002@gmail.com>
2023-06-01 22:30:31 +0530

    fixed search modal undefined bug

--- src/components/SearchModal/SearchModal.tsx
+++ src/components/SearchModal/SearchModal.tsx
@@ -4,9 +4,12 @@ import { motion } from "framer-motion";
 import { Search as SearchIcon, X } from "tabler-icons-react";
 
 import { useStore } from "@/store/globalStore";
+
+import { TreeNode } from "@/types/TreeNode";
 import { PathSegment } from "@/types/PathSegment";
-import { Backdrop } from "@/components/Common/Backdrop";
 import { firstLetterToUpperCase } from "@/utils/string";
+
+import { Backdrop } from "@/components/Common/Backdrop";
 
 const variants = {
   hidden: {
@@ -40,18 +43,17 @@ export default function SearchModal({ close }: { close: () => void }) {
     return lesson.label.toLowerCase().includes(search.toLowerCase());
   });
 
-  const MotionDivProps = {};
+  const MotionDivProps = {
+    exit: "exit",
+    initial: "hidden",
+    animate: "visible",
+    variants: variants,
+    className: "fixed z-50 inset-0 w-full flex justify-center bg-[#000000E1] py-12 px-4",
+  };
 
   return (
     <Backdrop onClick={close}>
-      <motion.div
-        className="fixed z-50 inset-0 w-full flex justify-center bg-[#000000E1] py-12 px-4"
-        exit="exit"
-        initial="hidden"
-        animate="visible"
-        variants={variants}
-        onClick={(e: React.MouseEvent<HTMLElement>) => e.stopPropagation()}
-      >
+      <motion.div {...MotionDivProps} onClick={(e: React.MouseEvent<HTMLElement>) => e.stopPropagation()}>
         <div className="flex flex-col max-w-3xl w-full">
           <div className="flex items-center justify-between text-gray-300 border-b border-outline bg-primary px-6 py-4 rounded-t-md">
             <div className="flex items-center gap-4">
@@ -71,29 +73,26 @@ export default function SearchModal({ close }: { close: () => void }) {
             </button>
           </div>
           <div className="flex flex-col gap-2 py-4 h-96 overflow-y-auto bg-primary rounded-b-md">
-            {filteredLessons.map((lesson) => {
-              if (!lesson.urlPath) return null;
-              return (
-                <Link
-                  key={lesson.urlPath}
-                  href={lesson.urlPath}
-                  className="hover:bg-secondary px-6 py-2"
-                  onClick={close}
-                >
-                  <span className="text-sm text-gray-400">
-                    {lesson.pathSegments.length === 1
-                      ? "Overview"
-                      : lesson.pathSegments
-                          .slice(0, -1)
-                          .map((segment: PathSegment) => firstLetterToUpperCase(segment.pathName))}
-                  </span>
-                  <h1 className="text-gray-200 font-medium">{lesson.title}</h1>
-                </Link>
-              );
-            })}
+            {filteredLessons.map((lesson) => (
+              <LessonLink key={lesson.pathName} lesson={lesson} />
+            ))}
           </div>
         </div>
       </motion.div>
     </Backdrop>
+  );
+}
+
+function LessonLink({ lesson }: { lesson: TreeNode }) {
+  if (!lesson.urlPath || !lesson.pathSegments) return null;
+  return (
+    <Link key={lesson.urlPath} href={lesson.urlPath} className="hover:bg-secondary px-6 py-2" onClick={close}>
+      <span className="text-sm text-gray-400">
+        {lesson.pathSegments?.length === 1
+          ? "Overview"
+          : lesson.pathSegments.slice(0, -1).map((segment: PathSegment) => firstLetterToUpperCase(segment.pathName))}
+      </span>
+      <h1 className="text-gray-200 font-medium">{lesson.title}</h1>
+    </Link>
   );
 }
