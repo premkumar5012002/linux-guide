5a82c9dbdeb9beb0aa2a5343f991455274e554d6
Prem Kumar <premkumar5012002@gmail.com>
2023-07-08 16:47:30 +0530

    [Replaced] Contentlayer to nextra

---
+++ src/theme.config.tsx
@@ -0,0 +1,8 @@
+import Logo from "@/components/Logo";
+
+export default {
+	logo: <Logo />,
+	project: {
+		link: "https://github.com/premkumar5012002/linux-guide",
+	},
+};
--- src/utils/string.ts
+++
@@ -1,6 +0,0 @@
-export function firstLetterToUpperCase(string: string) {
-  return string
-    .split("-")
-    .map((text) => text.charAt(0).toUpperCase() + text.slice(1))
-    .join(" ");
-}
