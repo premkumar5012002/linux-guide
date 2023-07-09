70443e2a137737ae8a678f11bcdbc06f7c249cd9
Prem Kumar <premkumar5012002@gmail.com>
2023-07-09 08:44:15 +0530

    [Added] History of Linux Lesson

--- src/theme.config.tsx
+++ src/theme.config.tsx
@@ -1,8 +1,42 @@
-import Logo from "@/components/Logo";
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
-export default {
-	logo: <Logo />,
+const config = {
+	logo: (
+		<div className="flex items-center gap-2">
+			<Image src="/linux.svg" alt="Linux Guide Logo" width={35} height={35} />
+			<p className="text-lg font-semibold">
+				Linux<span className="text-blue-500">guide</span>
+			</p>
+		</div>
+	),
+
 	project: {
 		link: "https://github.com/premkumar5012002/linux-guide",
 	},
+
+	docsRepositoryBase: "https://github.com/premkumar5012002/linux-guide/tree/main",
+
+	useNextSeoProps() {
+		const { asPath } = useRouter();
+		if (asPath !== "/") {
+			return {
+				titleTemplate: "%s – Linux Guide",
+			};
+		}
+	},
+
+	footer: {
+		text: (
+			<span>
+				{`MIT ${new Date().getFullYear()} © `}
+				<Link href="https://github.com/premkumar5012002" target="_blank" className="hover:text-blue-500">
+					Prem Kumar
+				</Link>
+			</span>
+		),
+	},
 };
+
+export default config;
--- src/utils/lessons.ts
+++ src/utils/lessons.ts
@@ -1,42 +1,17 @@
-import {
-	IconCpu,
-	IconTool,
-	IconHelp,
-	IconLock,
-	IconPower,
-	IconGizmo,
-	IconShare,
-	IconFolder,
-	IconRocket,
-	IconRouter,
-	IconNetwork,
-	IconRefresh,
-	IconServer2,
-	IconChartPie,
-	IconCodeDots,
-	IconPackages,
-	IconUserEdit,
-	IconServerCog,
-	IconTerminal2,
-	IconTextResize,
-	IconFilePencil,
-	IconCloudDataConnection,
-} from "@tabler/icons-react";
+import { IconHistory } from "@tabler/icons-react";
 
 export interface Lesson {
 	title: string;
 	excerpt: string;
+	url: string;
 	icon: React.ElementType;
 }
 
-export const terminal: Lesson[] = [
+export const basics: Lesson[] = [
 	{
 		title: "History of Linux",
 		excerpt: "What is Linux? Who created it? Why was it created? This lesson will answers all of these questions.",
-		icon: IconRocket,
+		url: "/lessons/history",
+		icon: IconHistory,
 	},
 ];
-
-export const system = [];
-
-export const networking = [];
